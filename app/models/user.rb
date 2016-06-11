c2b8f1a02a96266f8e714f69c953a909bed969f1
Tyler Pham <tylerpham.9@gmail.com>
2016-06-11 23:39:58 +0000

    password securing

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ActiveRecord::Base
     # Checks username and email are not blank 
     validates :username, presence: true
     validates :email, presence: true
+    
     #
     has_secure_password
+    validates :password, presence: true, length: { minimum: 6 }
 end
