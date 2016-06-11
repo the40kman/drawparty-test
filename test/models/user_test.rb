c2fe183ec9300d9df380218f7b3a53e68019c792
Tyler Pham <tylerpham.9@gmail.com>
2016-06-11 22:35:00 +0000

    check if username and email are present

---
+++ test/models/user_test.rb
@@ -0,0 +1,21 @@
+require 'test_helper'
+
+class UserTest < ActiveSupport::TestCase
+
+  def setup
+    @user = User.new(username: "Example User", email: "user@example.com")
+  end
+
+  test "should be valid" do
+    assert @user.valid?
+  end
+  
+  test "username should be present" do
+    @user.username = "     "
+    assert_not @user.valid?
+  end
+  test "email should be present" do
+    @user.email = "     "
+    assert_not @user.valid?
+  end
+end
