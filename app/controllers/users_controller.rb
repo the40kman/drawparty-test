a12c5cf16cf2e9843ca9bb16b2dffb072deefacb
Tyler Pham <tylerpham.9@gmail.com>
2016-06-12 19:44:33 +0000

    user signup without validation/testing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,23 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
   def new
+    @user = User.new
   end
+  def create
+    @users = User.new(users_params)
+    
+    respond_to do |format|
+      if @users.save
+        format.html { redirect_to @users, notice: 'Student was successfully created.' }
+        format.json { render :show, status: :created, location: @users }
+      else
+        format.html { render :new }
+        format.json { render json: @users.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+  private
+    def users_params
+      params.require(:user).permit(:username, :email, :password, :password_confirmation)
+    end
 end
