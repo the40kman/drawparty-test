896a61a6343fdc91eb2a4f128512a03800247cf5
Tyler Pham <tylerpham.9@gmail.com>
2016-06-14 03:30:34 +0000

    add home page and navigation bar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
     
     respond_to do |format|
       if @user.save
-        format.html { redirect_to @user, notice: 'Student was successfully created.' }
+        format.html { redirect_to @user, notice: 'Profile was successfully created.' }
         format.json { render :show, status: :created, location: @user }
       else
         format.html { render :new }
