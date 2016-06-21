1257f9ace781de1d2a96b6c45433eb0dcc6b229e
Tyler Pham <tylerpham.9@gmail.com>
2016-06-21 06:34:00 +0000

    change :bool to :boolean in admin

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       log_in user
-      if user.admin_user == 't'
+      if user.admin_user
         @user = User.all
         render 'users/admin'
       else
